acd9902c1e54b8f2059156367f660f8616708d43
natan.fagundes <natan.fags@gmail.com>
2023-03-07 11:49:46 -0300

    implementado type, criado rotas e ajuste no hadler de erros

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,11 @@ import {logger} from "./util.js"
 const {
     location,
     pages: {
-        homeHTML
+        homeHTML,
+        controllerHTML
+    },
+    constants: {
+        CONTENT_TYPE
     }
 } = config
 
@@ -18,6 +22,7 @@ async function routes(request, response) {
         response.writeHead(302, {
             'Location': location.home
         })
+
         return response.end()
     }
 
@@ -25,13 +30,53 @@ async function routes(request, response) {
         const {
             stream
         } = await controller.getFileStream(homeHTML)
+
         return stream.pipe(response)
     }
 
-    return response.end('hello')
+    if (method === 'GET' && url === '/controller') {
+        const {
+            stream
+        } = await controller.getFileStream(controllerHTML)
+
+        return stream.pipe(response)
+    }
+
+    if (method === 'GET') {
+
+        const {
+            stream,
+            type
+        } = await controller.getFileStream(url)
+
+        const contentType = CONTENT_TYPE[type]
+
+        if (contentType) {
+            response.writeHead(200, {
+                'Content-Type': contentType
+            })
+        }
+
+        return stream.pipe(response);
+    }
+
+    response.writeHead(404)
+    return response.end()
+}
+
+function handleError(error, response) {
+    if (error.message.includes('ENOENT')) {
+        logger.warn(`asset not found ${error.stack}`)
+        response.writeHead(404)
+        return response.end()
+    }
+
+    logger.error(`caught error on API ${error.stack}`)
+    response.writeHead(500)
+    return response.end()
 }
 
 export function handler(request, response) {
     return routes(request, response)
-        .catch(error => logger.error(`Deu ruim: ${error.stack}`))
+        .catch(error => handleError(error, response))
 }
